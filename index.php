035b0bebd8fa72e904ad837ca54ba2545e15e787
Jason Gordy <d2king10@gmail.com>
2015-12-04 12:11:49 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html dir="ltr" lang="en-US">
+    <head>
+        <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="author" content="Planling">
+
+        <title>Planling</title>
+
+        <link href="styles/reset.css" rel="stylesheet" type="text/css" />
+        <link href="styles/main.css" rel="stylesheet" type="text/css" />
+        <link href="styles/responsive.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+    </head>
+	<body>
+		<div id="header">
+			<article>
+				<a class="logo" href=""><img src="images/header_logo.png" alt="Planling"></a>
+				<nav>
+					<ul class="inline">
+						<li class="home"><a href="" class="fa fa-home"></a></li>
+						<li class="create"><a href="" class="fa fa-book">&nbsp;&nbsp;Create</a></li>
+						<li class="explore"><a href="" class="fa fa-star">&nbsp;&nbsp;Explore</a></li>
+						<li class="alerts"><a href="" class="fa fa-envelope"></a></li>
+						<li class="help"><a href="" class="fa fa-question"></a></li>
+						<li class="avatar"><a href=""><img src="images/test_avatar.jpg"></a></li>
+						<li class="more"><a href="" class="fa fa-caret-down"></a></li>
+					</ul>
+				</nav>
+			</article>
+		</div>
+
+		<div id="latest-shell">
+			<article id="latest">
+				<h1>Your upcoming lessons</h1>
+				<table>
+					<tr>
+						<th></th>
+						<th>day</th>
+						<th>time</th>
+						<th>class</th>
+						<th>lesson</th>
+						<th>description</th>
+					</tr>
+				</table>
+			</article>
+		</div>
+
+	</body>
+</html>
